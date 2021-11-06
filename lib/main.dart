2ff519d7ad10b277b87fac90985be6c0e7cee837
Yeongse Kim <74057052+Yeongse@users.noreply.github.com>
2021-11-06 17:38:21 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-import 'ui/ BlackBoard.dart';
-import 'ui/ LessonLogging.dart';
-import 'ui/ SpeakLog.dart';
-import 'ui/ LogUnit.dart';
-import 'ui/ TopMenu.dart';
+import 'ui/BlackBoard.dart';
+import 'ui/LessonLogging.dart';
+import 'ui/SpeakLog.dart';
+import 'ui/LogUnit.dart';
+import 'ui/TopMenu.dart';
 
 void main() => runApp( new MaterialApp(
   home: LessonLogging(),
