2fcc6defd640333d32f8e6ff5bd980e45f4fb687
yamaAo0130 <yamaguchiaoi0130@gmail.com>
2021-11-07 01:38:49 +0900

    音声処理の実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,54 @@
 import 'package:flutter/material.dart';
 
-import 'ui/ BlackBoard.dart';
-import 'ui/ LessonLogging.dart';
-import 'ui/ SpeakLog.dart';
-import 'ui/ LogUnit.dart';
-import 'ui/ TopMenu.dart';
+import 'ui/BlackBoard.dart';
+import 'ui/LessonLogging.dart';
+import 'ui/SpeakLog.dart';
+import 'ui/LogUnit.dart';
+import 'ui/TopMenu.dart';
 
-void main() => runApp( new MaterialApp(
-  home: LessonLogging(),
-));
+import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_recognition_error.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int number = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter入門アカデミー'),
+          backgroundColor: Colors.orange,
+        ),//////////////
+
+        body: SpeakLog(context),
+        // body: Center(
+        //   child: Text(
+        //     'Hello World',
+        //     style: TextStyle(
+        //       fontSize: 40,
+        //     ),
+        //   ),
+        // ),
+
+      ),
+    );
+  }
+}
+
+
+
+
