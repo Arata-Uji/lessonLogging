b3e4f7a096ab8c454225389b79a90a7984c1c680
Arata-Uji <sep29h12@gmail.com>
2021-11-06 22:42:43 +0900

    my first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-import 'ui/ BlackBoard.dart';
-import 'ui/ LessonLogging.dart';
-import 'ui/ SpeakLog.dart';
-import 'ui/ LogUnit.dart';
-import 'ui/ TopMenu.dart';
+import 'ui/BlackBoard.dart';
+import 'ui/LessonLogging.dart';
+import 'ui/SpeakLog.dart';
+import 'ui/LogUnit.dart';
+import 'ui/TopMenu.dart';
 
 void main() => runApp( new MaterialApp(
-  home: LessonLogging(),
+  home: TopMenu(),
 ));
