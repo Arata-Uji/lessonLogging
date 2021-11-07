2b44aadf11dc73e020e353f26cf62dd52ecb5923
Yeongse Kim <74057052+Yeongse@users.noreply.github.com>
2021-11-07 14:39:56 +0900

    Update SpeakLog.dart

--- lib/ui/SpeakLog.dart
+++ lib/ui/SpeakLog.dart
@@ -1,53 +1,22 @@
-import 'package:flutter/material.dart';
-import 'package:speech_to_text/speech_recognition_error.dart';
-import 'package:speech_to_text/speech_recognition_result.dart';
-import 'package:speech_to_text/speech_to_text.dart' as stt;
-//
-//
-Column SpeakLog(BuildContext context){
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.end,
-    children: <Widget>[
-      Padding(
-        padding: const EdgeInsets.all(2.0),
-        child: Text(""),
-      ),
-    ],
-  );
-}
+// import 'package:flutter/material.dart';
+// import 'package:speech_to_text/speech_recognition_error.dart';
+// import 'package:speech_to_text/speech_recognition_result.dart';
+// import 'package:speech_to_text/speech_to_text.dart' as stt;
+// //
+// //
 
 
-//以下音声認識のコード
+
+// //以下音声認識のコード
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_recognition_error.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 
-void main() {
-  runApp(MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, this.title}) : super(key: key);
-  final String? title;
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
 
-class _MyHomePageState extends State<MyHomePage> {
+class SpeakLog  {
   String lastWords = "";
   String lastError = '';
   String lastStatus = '';
@@ -68,49 +37,26 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void resultListener(SpeechRecognitionResult result) {
-    setState(() {
+
       lastWords = '${result.recognizedWords}';
-    });
+
   }
 
   void errorListener(SpeechRecognitionError error) {
-    setState(() {
+
       lastError = '${error.errorMsg} - ${error.permanent}';
-    });
+
   }
 
   void statusListener(String status) {
-    setState(() {
+
       lastStatus = '$status';
-    });
+
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title!),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '変換文字:$lastWords',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Text(
-              'ステータス : $lastStatus',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton:
-      Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-        FloatingActionButton(onPressed: _speak, child: Icon(Icons.play_arrow)),
-        FloatingActionButton(onPressed: _stop, child: Icon(Icons.stop))
-      ]),
-    );
+  String Voicerecord(){
+    _speak();
+    _stop();
+    return lastStatus;
   }
 }
