6fd93dda3cf9315fa9097ba06931a4f8774665a4
skrend <seiyamasato.kobe@gmail.com>
2021-11-07 10:11:10 +0900

    LessonLoggingを変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 
-import 'ui/ BlackBoard.dart';
-import 'ui/ LessonLogging.dart';
-import 'ui/ SpeakLog.dart';
-import 'ui/ LogUnit.dart';
-import 'ui/ TopMenu.dart';
+import 'ui/BlackBoard.dart';
+import 'ui/LessonLogging.dart';
+import 'ui/SpeakLog.dart';
+import 'ui/LogUnit.dart';
+import 'ui/TopMenu.dart';
 
-void main() => runApp( new MaterialApp(
-  home: LessonLogging(),
-));
+void main() {
+  runApp(
+      MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LessonLogging()
+    );
+  }
+}
