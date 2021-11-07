322363c4563f879d807089e64fde725b1565cbe2
Yeongse <yeongsekm@gmail.com>
2021-11-07 10:44:30 +0900

    re commit by yeongse

---
+++ lib/next_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Nextpage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('LessonLogging'),
+      ),
+      body: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+
+}
---
+++ lib/ui/TopMenu.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:lesson_logging/next_page.dart';
+
+class TopMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('LessonLogging'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('授業を開始する'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Nextpage(),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
